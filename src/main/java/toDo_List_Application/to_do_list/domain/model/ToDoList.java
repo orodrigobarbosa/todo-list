e97689ebad25eb55154833405d6d8f742d243569
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-07-30 17:26:57 -0300

    first commit

---
+++ src/main/java/toDo_List_Application/to_do_list/domain/model/ToDoList.java
@@ -0,0 +1,21 @@
+package toDo_List_Application.to_do_list.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ToDoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String status;
+}
