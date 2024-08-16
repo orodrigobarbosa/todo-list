5499a42cff9a3a64a2514cf85f3808ec7a5fb4b3
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-08-16 19:14:14 -0300

    Validação Status de Tarefa

--- src/main/java/toDo_List_Application/to_do_list/domain/handler/HandleIDNaoEncontrado.java
+++ src/main/java/toDo_List_Application/to_do_list/domain/handler/HandleIDNaoEncontrado.java
@@ -1,8 +1,9 @@
 package toDo_List_Application.to_do_list.domain.handler;
 
-public class HandleIDNaoEncontrado extends RuntimeException{
+public class HandleIDNaoEncontrado extends RuntimeException {
 
-    public HandleIDNaoEncontrado(String message){
+    public HandleIDNaoEncontrado(String message) {
         super(message);
     }
 }
+
